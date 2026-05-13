52dbc52f1a801e520f5e08a626262a4e918f05e2
Paolo Ragudo <ragudopaolo@gmail.com>
2026-05-13 19:35:20 +0800

    feat: supbase setup, email auth, and proxy (middleware)

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,10 +2,27 @@
 
 import React from "react";
 
+// check if user session exists
+// uncomment the imports and the client and additional checks
+// and make FeedLayout async
+
+// import { createClient } from "@/lib/supabase/server"; //
+// import { redirect } from "next/navigation";
+
+
 export default function FeedLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	// const supabase = await createClient();
+    
+	// const { data: { user } } = await supabase.auth.getUser();
+
+	// // If no user is found, redirect to the login page immediately
+	// if (!user) {
+	// 	redirect("/login");
+	// }
+
 	return <div>{children}</div>;
 }
